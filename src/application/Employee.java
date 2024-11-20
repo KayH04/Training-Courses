3867ce2c153c65b859c9bcfea6395454897b16bc
Kay Hickey <c23434334@mytudublin.ie>
2024-11-20 16:36:34 +0000

    second commit

--- src/application/Employee.java
+++ src/application/Employee.java
@@ -1,72 +1,72 @@
 package application;
 
-public class Employee {
-	
-	private int employeeNumber;
-	private String employeeName;
-	private int yearsWorked;
-	private String courseName;
-	
-	public Employee() {
-		this.employeeNumber = 0;
-		this.employeeName = "";
-		this.yearsWorked = 0;
-		this.courseName ="";
-	}
-	
-	public Employee (int num, String name, int years, String course, int yearsWorked, String courseName) {
-		this.employeeNumber = num;
-		this.employeeName = name;
-		this.yearsWorked = years;
-		this.courseName = course;
+	public class Employee {
 		
-		if (yearsWorked > 5) 
-	        this.yearsWorked = yearsWorked;
-	    else
-	        System.out.println("Employee must have more than 5 years of experience.");
-	
+		private int employeeNumber;
+		private String employeeName;
+		private int yearsWorked;
+		private String courseName;
 		
-	    // Validate course name
-	    if (courseName.startsWith("FOOD")) 
-	        this.courseName = courseName;
-		 else 
-	    
-	        System.out.println("Course name invalid. Setting to ERROR.");
-	        this.courseName = "ERROR"; 
-	}
-	
-	public int getNum() {
-		return this.employeeNumber;
-	}
-	
-	public String getName() {
-		return this.employeeName;
-	}
-	public int getYears() {
-		return this.yearsWorked;
-	}
-	public String getCourse() {
-		return this.courseName;
-	}
-	
+		public Employee() {
+			this.employeeNumber = 0;
+			this.employeeName = "";
+			this.yearsWorked = 0;
+			this.courseName ="";
+		}
+		
+		public Employee (int num, String name, int years, String course) {
+			this.employeeNumber = num;
+			this.employeeName = name;
+			this.yearsWorked = years;
+			this.courseName = course;
+			
+			if (yearsWorked > 5) 
+		        this.yearsWorked = years;
+		    else
+		        System.out.println("Employee must have more than 5 years of experience.");
+		
+			
+		    // Validate course name
+		    if (courseName.startsWith("FOOD")) 
+		        this.courseName = course;
+			 else 
+		    
+		        System.out.println("Course name invalid. Setting to ERROR.");
+		        this.courseName = "ERROR"; 
+		}
+		
+		public int getNum() {
+			return this.employeeNumber;
+		}
+		
+		public String getName() {
+			return this.employeeName;
+		}
+		public int getYears() {
+			return this.yearsWorked;
+		}
+		public String getCourse() {
+			return this.courseName;
+		}
+		
 
-	public void setNum(int num) {
-		this.employeeNumber = num;
-	}
-	public void setName(String name) {
-		this.employeeName = name;
-	}
-	public void setYears(int years) {
-		this.yearsWorked = years;
-	}
-	public void setCourse(String course) {
-		this.courseName = course;
-	}
-	
+		public void setNum(int num) {
+			this.employeeNumber = num;
+		}
+		public void setName(String name) {
+			this.employeeName = name;
+		}
+		public void setYears(int years) {
+			this.yearsWorked = years;
+		}
+		public void setCourse(String course) {
+			this.courseName = course;
+		}
+		
 
-	public String toString() {
-		return "Employees number " + this.employeeNumber + "\n"
-				+ "Employee name " + this.employeeName + "\n"
-				+ "Years worked by employee " + this.yearsWorked + "\n";
+		public String toString() {
+			return "Employees number " + this.employeeNumber + "\n"
+					+ "Employee name " + this.employeeName + "\n"
+					+ "Years worked by employee " + this.yearsWorked + "\n";
+		}
 	}
-}
--- src/application/TrainingCourses.java
+++ src/application/TrainingCourses.java
@@ -1,82 +1,123 @@
 package application;
+import util.LinearNode;
 import util.LinkedList;
 import java.util.Scanner;
+	
 public class TrainingCourses {
-	
-		LinkedList<Employee>list;
 		
-	public TrainingCourses() {
-		list = new LinkedList<Employee>();
-		inputEmployees();
-	}
-
-	private void inputEmployees() { //1. Allow users to input 10 employees - ask how many employees they wish to enter first 
-		// TODO Auto-generated method stub
-		Scanner scan = new Scanner(System.in);
-		employee e;
-		int employeeNumber;
-		String employeeName;
-		int yearsWorked;
-		String courseName;
-		int count;
-		
-		int input;
-		System.out.println("Please enter how many employees you would like to add : ");
-		input = scan.nextInt(); scan.nextLine();
-		
-		
-		while (input<= 10) 
+	LinkedList<Employee>list;
 			
-			System.out.println("Please enter the employees datails ");
-			
-		for (int count=1; count <=10; count++)
-		
-			System.out.println("Please enter the employees number: ");
-			employeeNumber = scan.nextInt(); scan.nextLine();
-		    System.out.print("\nEnter name for module " + count + " : ");
-		    employeeName = scan.nextLine();
-		    System.out.print("Enter code for module " + count + " : ");
-		    yearsWorked  = scan.nextInt();scan.nextLine();
-		    System.out.print("Enter number of credits for module " + count + " : ");
-		    courseName = scan.nextLine();
-
-		  //Create an employee
-		    e = new Employee(employeeNumber, employeeName, yearsWorked, courseName);
-
-		    //Add to LinkedList
-		    list.add(e);
-			
-		
-		
-		
-		if(input >10) 
-			System.out.println("You have chosen too many employees to enter - please choose a smaller number :");
-			input = scan.nextInt(); scan.nextLine(); 
-		
-			displayEmployees();
+public TrainingCourses() {
+			list = new LinkedList<Employee>();
+			inputEmployees();
 		}
+
+public void inputEmployees() { //1. Allow users to input 10 employees - ask how many employees they wish to enter first 
+			// TODO Auto-generated method stub
+			Scanner scan = new Scanner(System.in);
+			Employee emp;
+			int employeeNumber;
+			String employeeName;
+			int yearsWorked;
+			String courseName;
+			
+			
+			int input;
+			System.out.println("Please enter how many employees you would like to add : ");
+			input = scan.nextInt(); scan.nextLine();
+			
+			
+			while (input > 10 || input < 1) {
+			    System.out.println("You have chosen too many employees to enter - please choose a smaller number:");
+			    input = scan.nextInt();
+			    scan.nextLine(); 
+			}
+				
+			for (int count=1; count<=input; count++) 
+			
+				System.out.println("Please enter the employees number: ");
+				employeeNumber = scan.nextInt(); 
+				scan.nextLine();
+			    System.out.print("\nEnter name for module " + count + " : ");
+			    employeeName = scan.nextLine();
+			    System.out.print("Enter code for module " + count + " : ");
+			    yearsWorked  = scan.nextInt();
+			    scan.nextLine();
+			    System.out.print("Enter number of credits for module " + count + " : ");
+			    courseName = scan.nextLine();
+
+			  //Create an employee
+			    emp = new Employee(employeeNumber, employeeName, yearsWorked, courseName);
+
+			    //Add to LinkedList
+			    list.add(emp);
+				
+				displayEmployees();
+			}
+			
+		
+	public void displayEmployees(){//2. Display all employee details
+		if (list.isEmpty()) 
+			System.out.println("No employees to display.");
+		 else 
+			System.out.println("Employee Details:");
+			list.display(); 
+			removeEmployee();
+		
+		}
+	public void removeEmployee() {//3. Remove an employee from a course - using employee number 
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the employee number to remove: ");
+		int employeeNumber = scan.nextInt();
+		
+		LinearNode<Employee> current = list.getFirstNode();
+		boolean found = false;
+		while (current!=null) {
+			if(current.getElement().getNum()== employeeNumber) {
+				list.delete(current.getElement());
+				System.out.println("Employee removed ");
+				found = true;
+				break;
+			}
+			current = current.getNext();
+		}
+		displayEmployeesInTraining();
+		
+	}
+		
+		
+	public void displayEmployeesInTraining(){	//4. Display all details of employees in training 
+		
+		if(list.isEmpty()) {
+			System.out.println("No emploees are enrolled in any course. ");
+		} else {
+			LinearNode<Employee> current =list.getFirstNode();
+		
+		while (current!=null) {
+			Employee emp = current.getElement();
+																																										if(!emp.getCourse().equals("ERROR")) 
+				System.out.println(emp);
+			
+		}
+		current = current.getNext();
+		}
+	}
+		
 		
 	
-	private void displayEmployees(){//2. Display all employee details
 		
 		
-	
-	}
-	//3. Remove an employee from a course - using employee number 
-	
-	//4. Display all details of employees in training 
-	
-	//5. Delete all employees from a specific course 
-	
-	//6. Display remaining employees 
-	
-	//7. Personal Function 
-	
-	
+		//5. Delete all employees from a specific course 
+		
+		//6. Display remaining employees 
+		
+		//7. Personal Function 
+		
+		
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
 
-	}
+		}
 
-	}
+		}
